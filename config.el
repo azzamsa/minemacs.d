542b6564eff3abbefa0b6e24c80b26d45209fc9a
azzamsa <me@azzamsa.com>
2023-06-21 23:24:19 +0700

    fix: don't fails in non project directory

--- config.el
+++ config.el
@@ -180,14 +180,15 @@
 (defun neotree-project-dir ()
   "Always open NeoTree in project root."
   (interactive)
-  (let ((project-dir (project-root (project-current)))
+  (let ((project-dir (ignore-errors (project-root (project-current))))
         (file-name (buffer-file-name)))
     (neotree-toggle)
     (if project-dir
         (if (neo-global--window-exists-p)
             (progn
               (neotree-dir project-dir)
-              (neotree-find file-name)))
+              (if file-name
+                  (neotree-find file-name))))
       (message "Could not find project root."))))
 
 ;;; :tools magit
